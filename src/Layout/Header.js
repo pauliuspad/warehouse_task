cff5f441be63d0d382b4dfc26ae7a133a4d86a69
pauliuspad@gmail.com <pauliuspad@gmail.com>
2020-03-09 13:46:20 +0200

    Added Links

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
       <header style={headerStyle}>
           <h1>Warehouse App</h1>
+          <Link to="/products">Products</Link> |  <Link to="/products/create">Create new</Link>
       </header>
   )
 }
